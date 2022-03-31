cba0617de38b0e7c3bbaa165e8b3fe93a7437e72
joshmin-tech <joshminrs@gmail.com>
2022-03-31 09:19:47 +0530

    feat: add property slice and fixed conflicts

--- client/src/pages/PropertiesPage.jsx
+++ client/src/pages/PropertiesPage.jsx
@@ -2,18 +2,22 @@ import React, { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Spinner from "../components/Spinner";
+import { getProperties } from "../features/properties/propertySlice";
 
 const PropertiesPage = () => {
+	const { properties, isLoading, isSuccess } = useSelector(
+		(state) => state.properties
+	);
 
-	// const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-	// useEffect(() => {
-	// 	dispatch(getProperties());
-	// }, [dispatch]);
+	useEffect(() => {
+		dispatch(getProperties());
+	}, [dispatch]);
 
-	// if (isLoading) {
-	// 	return <Spinner />;
-	// }
+	if (isLoading) {
+		return <Spinner />;
+	}
 	return (
 		<>
 			<Container>
